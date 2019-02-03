1993fdf0235f5d9980891cf567e468133fd4e47c
kslinguistics <47296704+kslinguistics@users.noreply.github.com>
2019-02-03 22:24:23 +0900

    Add files via upload
    added spell checker

---
+++ spellchecker.cpp
@@ -0,0 +1,83 @@
+/**** simple spell checker ****/
+
+#include <bits/stdc++.h>
+
+struct ToLower{
+    char operator()(char c){
+		return std::tolower(c); 
+	}
+};
+
+// calculate Levenshtein distance between s1 and s2
+int d(std::string s1, std::string s2)
+{
+	int m = int(s1.size()), n = int(s2.size());
+	int dp[20][20] = {};
+	for(int i = 0; i != m + 1; i++){
+		for(int j = 0; j != n + 1; j++){
+			dp[i][j] = i + j;
+			if(i > 0)	dp[i][j] = std::min(dp[i][j], dp[i - 1][j] + 1);
+			if(j > 0)	dp[i][j] = std::min(dp[i][j], dp[i][j - 1] + 1);
+			if(i > 0 && j > 0){
+				dp[i][j] = std::min(dp[i][j], dp[i - 1][j - 1] + 1 * (s1[i - 1] != s2[j - 1]));
+			}
+			if(i > 1 && j > 1){
+				if(s1[i - 1] == s2[j - 2] && s1[i - 2] == s2[j - 1]){
+					dp[i][j] = dp[i - 2][j - 2] + 1;
+				}
+			}
+		}
+	}
+	return dp[m][n];
+}
+
+// return int(log_5(n))
+int logpoor5(int n){
+	int l = 0;
+	if(n > 5)        l = 1;
+	if(n > 25)       l = 2;
+	if(n > 125)      l = 3;
+	if(n > 625)      l = 4;
+	if(n > 3125)     l = 5;
+	if(n > 15625)    l = 6;
+	if(n > 78125)    l = 7;
+	if(n > 390625)   l = 8;
+	if(n > 1953125)  l = 9;
+	if(n > 9765625)  l = 10;
+	return l;
+}
+
+int main(void)
+{
+	// read dictionary data
+	// words are recorded in the form of '(word) (frequency)'
+	std::map<std::string, int> dict;
+	std::ifstream words("en_110k.txt");
+	std::string str;
+	std::vector<std::string> data;
+	while(getline(words, str)){
+		int pos = str.find(' ');
+		dict[str.substr(0, pos)] = logpoor5(std::atoi(str.substr(pos + 1).c_str()));
+	}
+	
+	// find words with correct spelling
+	while(true){
+		int score = -100, score_tmp = -100, d_tmp = 0;
+		auto itr_max = dict.begin();
+		std::cin >> str;
+		std::transform(str.begin(), str.end(), str.begin(), ToLower());
+		for (auto itr = dict.begin(); itr != dict.end(); itr++){
+			// variable score_tmp means
+			// p((correct spelling) | (wrong spelling))
+			d_tmp = d(str, itr->first);
+			score_tmp = 2 * itr->second - 7 * d_tmp;
+			if(score_tmp > score && itr->second != 0){
+				itr_max = itr;
+				score = score_tmp;
+			}
+		}
+		
+		std::cout << itr_max->first << " ";
+	}
+	return 0;
+}
